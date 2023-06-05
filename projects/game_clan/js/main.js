533983ef45dc9aae4ce62ce5c2c997ada07154ee
Ihor Panov <42876401+ihorpanov@users.noreply.github.com>
2023-06-05 20:20:41 +0300

    Delete projects directory

--- projects/game_clan/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$(document).on("click", "button[name='send']", function() {
-		var message = $("textarea[name='inp']").val();
-		$("<div class='msg'><div class='msg_top clear'><a class='photo'></a><p class='nick'><a href='#' class='nick_a'>Unknown User</a></p><p class='time'>12:00</p></div><p class='text'>"+message+"</p></div>").prependTo("#chat");
-		document.getElementById("textarea").value = "";
-	});
-});
